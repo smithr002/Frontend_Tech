84734d00563dfec5a571e6ecc3810f9f96f61168
Robyn Smith <robyn.l.smith1999@gmial.com>
2019-06-25 13:51:24 +0200

    alles

---
+++ java.js
@@ -0,0 +1,9 @@
+
+document.getElementById('continue').addEventListener('click', function (){
+    document.getElementById("progressBar").value = "33";
+    window.location.href="#form2"
+})
+document.getElementById('continue1').addEventListener('click', function (){
+    document.getElementById("progressBar").value = "66";
+    window.location.href="#form3"
+})
